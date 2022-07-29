41f15ce1dc47d2b6dfd9bc476e6f624f939f42ca
JakeBandy <jakebandy13@hotmail.com>
2022-07-28 19:24:57 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      todos: [{id: 1, todo:"walk fish"},
+              {id: 2, todo: "water roof"}, 
+              {id: 3, 'feed dragon'},
+            ],
+          },
+      text: ''
+    }
+
+  }
+
+  // handleClick = () => {
+  //   this.setState({isClicked: !this.state.isClicked})
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    this.setState({
+      todos: [...this.state.todos, this.state.text],
+      text: ''
+    })
+  }
+
+  handleDelete = (index) => {
+    console.log(index)
+    let copyOfList = this.state.todos
+    copyOfList.splice(index, 1)
+    this.setState({todos: [...copyOfList]})
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.todos)
+  }
+
+
+
+  render() {
+    return (
+      <div className="app">
+      <h2>Todos</h2>
+      <input type="text" onChange={this.handleChange} value={this.state.text}/>
+      {/* <button onClick={this.handleClick}>{this.isClicked ? 'Toggle' : 'Untoggle'}</button> */}
+      <button onClick={this.handleSubmit}>Submit</button>
+      <ul>
+      {
+        this.state.todos.map((todo, id) => {
+          return <li key={id}>{todo}</li>
+        })
+      }
+      </ul>
+      </div>
+    );
+  };
+
 
 export default App;
